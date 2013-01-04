8c91ccafe6b621e0e3d7eb9a654af849d00f5ea6
Josh Butts <josh@joshbutts.com>
2013-01-04 08:40:51 -0600

    add statsjob command

---
+++ lib/Bstools/Command/StatsJob.php
@@ -0,0 +1,31 @@
+<?php
+namespace Bstools\Command;
+use Symfony\Component\Console\Command\Command,
+    Symfony\Component\Console\Input\InputArgument,
+    Symfony\Component\Console\Input\InputOption,
+    Symfony\Component\Console\Input\InputInterface,
+    Symfony\Component\Console\Output\OutputInterface,
+    Bstools\Table;
+
+class StatsJob extends Base
+{
+    public function configure()
+    {
+        $this->setName('stats-job')
+             ->setDescription('Stats on a specific job id');
+        $this->addArgument('job-id', InputArgument::REQUIRED, 'the integer job id');
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $pheanstalk = new \Pheanstalk($input->getOption('host'));
+
+        $jobId = $input->getArgument('job-id');
+        $stats = $pheanstalk->statsJob($jobId);
+        var_dump($stats);
+        $output->writeln("<info>Job ID: </info> <comment>$jobId</comment>");
+        foreach ($stats as $key => $val) {
+            $output->writeln("<info>$key: </info><comment>$val</comment>");
+        };
+    }
+}
